711aa1cdea9a81f1aa1b51f931e70faab543bbea
Felipe (Home) <sharpion.k@gmail.com>
2022-04-28 23:12:42 +0100

    Fixing stories

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import { Story } from '@storybook/react';
-import { Button } from '../components/';
+import { Button as Component } from '../components/';
 import { ButtonProps } from '../components/types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 export default {
   title: 'Button',
-  component: Button
+  component: Component
 };
 
-const ButtonComponent: Story<ButtonProps> = (args) => <Button {...args} />;
-export const Buttons = () => (
-  <ButtonComponent
-    leftIcon
-    icon={<FontAwesomeIcon icon={faCoffee} size="lg" />}
-    isDisabled={false}
-    isDarkMode={false}
-    isSelected={false}
-    shadowed={false}
-    size="medium"
-    shape="squared"
-    variant="confirm"
-    onClick={() => alert('Click!')}
-  >
-    Primary
-  </ButtonComponent>
-);
+const Template: Story<ButtonProps> = (args) => <Component {...args} />;
+
+export const Button = Template.bind({});
+Button.args = {
+  children: 'Coffee',
+  leftIcon: true,
+  icon: <FontAwesomeIcon icon={faCoffee} size="lg" />,
+  isDisabled: false,
+  isDarkMode: false,
+  isSelected: false,
+  shadowed: false,
+  size: 'medium',
+  shape: 'squared',
+  variant: 'confirm',
+  onClick: () => alert('Click!')
+};
