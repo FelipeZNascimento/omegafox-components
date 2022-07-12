a3464f43811056730445ee14af56af360cf3d0b8
Felipe (Home) <sharpion.k@gmail.com>
2022-07-12 20:26:44 +0100

    fix github actions on dev + fix build on local

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,18 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 
-import postcss from "rollup-plugin-postcss";
+import postcss from 'rollup-plugin-postcss';
 import visualizer from 'rollup-plugin-visualizer';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: [
-    './src/index.tsx'
-  ],
+  input: ['./src/index.tsx'],
   output: {
     dir: 'dist',
     format: 'esm',
     preserveModules: true,
     preserveModulesRoot: 'src',
-    sourcemap: true,
+    sourcemap: true
   },
   plugins: [
     resolve(),
@@ -30,8 +28,8 @@ export default {
     terser(),
     visualizer({
       filename: 'bundle-analysis.html',
-      open: true,
-    }),
+      open: true
+    })
   ],
-  external: ['react', 'react-dom'],
+  external: ['react', 'react-dom']
 };
