3aa6e59e3949fcaf752644cbd9a1b1ac3edaabf4
Felipe (Home) <sharpion.k@gmail.com>
2022-07-12 21:39:18 +0100

    fix: merge

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 
 import postcss from 'rollup-plugin-postcss';
 import visualizer from 'rollup-plugin-visualizer';
@@ -22,7 +22,8 @@ export default {
       tsconfig: './tsconfig.build.json',
       declaration: true,
       declarationDir: 'dist',
-      resolveJsonModule: true
+      resolveJsonModule: true,
+      clean: true,
     }),
     postcss(),
     terser(),
