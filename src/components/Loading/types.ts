0dac281dc44c014613c5dae318d5d029b9b5923e
Felipe (Home) <sharpion.k@gmail.com>
2022-07-13 11:19:03 +0100

    Fix loading shadows

--- src/components/Loading/types.ts
+++ src/components/Loading/types.ts
@@ -1,6 +1,8 @@
 export interface LoadingProps {
   image: string;
   overlay?: boolean;
+  shadowed?: boolean;
   size?: 'small' | 'regular' | 'big';
   style?: 'spin' | 'headbutt';
+  text?: string;
 }
