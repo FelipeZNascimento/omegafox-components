1c0a4c3d50cdfdd312c00bc83ae9e2e62793f256
Felipe (Home) <sharpion.k@gmail.com>
2022-07-13 12:04:29 +0100

    Update: auto updates version on package.json on new release

--- release.config.js
+++ release.config.js
@@ -1,19 +1,27 @@
 module.exports = {
-    branches: ["master"],
-    plugins: [
-        [
-            "@semantic-release/commit-analyzer",
-            {
-                preset: "eslint",
-            },
-        ],
-        [
-            "@semantic-release/release-notes-generator",
-            {
-                preset: "eslint",
-            },
-        ],
-        "@semantic-release/npm",
-        "@semantic-release/github",
+  branches: ['master'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'eslint'
+      }
     ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'eslint'
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['dist/**/*.{js,css}', 'docs', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github'
+  ]
 };
