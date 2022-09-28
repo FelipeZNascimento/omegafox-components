dd8a865d859e85e7b787ab3df29ffec6cb5a9cb9
Felipe (Home) <sharpion.k@gmail.com>
2022-09-28 15:40:00 +0100

    Fix: Tooltip component

--- src/components/Tooltip/types.ts
+++ src/components/Tooltip/types.ts
@@ -1,6 +1,5 @@
 export interface ITooltipProps {
   children: JSX.Element;
-  position?: 'top' | 'right' | 'bottom' | 'left';
   text: string;
-  type?: 'dark' | 'success' | 'warning' | 'error' | 'info' | 'light';
+  type?: 'neutral' | 'success' | 'error';
 }
